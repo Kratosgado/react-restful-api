7a9d587213a6d07d25247e7e05e9d6d0d3a69473
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-08-04 13:54:20 +0000

    use react router data loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { getPosts } from './posts/getPosts';
+
 import './App.css';
 import { PostsPage } from './posts/PostsPage';
 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <PostsPage />,
+    loader: getPosts
+  }
+]);
+
 function App() {
-  return (
-    <PostsPage />
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/posts/PostsPage.tsx
+++ src/posts/PostsPage.tsx
@@ -1,39 +1,20 @@
 import { useEffect, useState } from 'react';
+import {useLoaderData} from 'react-router-dom'
+
 import { PostsList } from './PostsList';
 import { NewPostData, PostData } from './types';
-import { getPosts } from './getPosts';
+import { getPosts, assertIsPosts } from './getPosts';
 import { NewPostForm } from './NewPostForm';
 import { savePost } from './savePost';
 
 export function PostsPage() {
-   const [isLoading, setIsLoading] = useState(true);
-   const [posts, setPosts] = useState<PostData[]>([]);
-
-   useEffect(() => {
-      let cancel = false;
-      getPosts().then((data) => {
-         if (!cancel) {
-            setPosts(data);
-            setIsLoading(false);
-         }
-      });
-      return () => {
-         cancel = true
-      }
-   }, []);
+   const posts = useLoaderData();
+   assertIsPosts(posts)
 
    async function handleSave(newPostData: NewPostData) {
       const newPost = await savePost(newPostData);
-      setPosts([newPost, ...posts])
    }
 
-   if (isLoading) {
-      return (
-         <div className='w-96 mx-auto mt-6'>
-            Loading ...
-         </div>
-      )
-   }
 
    return (
       <div className='w-96 mx-auto mt-6'>
