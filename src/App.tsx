967b90731ee005625374f0260da8af89c98caea4
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-08-04 12:26:06 +0000

    create and display the PostsPage components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import './App.css';
-import { Header } from './Header';
-import { Outlet } from 'react-router-dom'
+import { PostsPage } from './posts/PostsPage';
 
 function App() {
   return (
-    <>
-      <Header />
-      <Outlet />
-    </>
+    <PostsPage />
   );
 }
 
